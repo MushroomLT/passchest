bd20c8443e216346bb8567bcd3962d9bb72b799d
Kasparas Eidukonis <kasparas6@hotmail.com>
2017-08-23 19:39:24 +0300

    Update project structure and add android app module

---
+++ android/src/main/java/com/passchest/passchest/android/AndroidDriveHelper.java
@@ -0,0 +1,9 @@
+package com.passchest.passchest.android;
+
+/**
+ * Created by Kasparas on 2017-08-23.
+ */
+
+public class AndroidDriveHelper {
+
+}
---
+++ android/src/main/java/com/passchest/passchest/android/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.passchest.passchest.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.passchest.passchest.DriveHelper;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
